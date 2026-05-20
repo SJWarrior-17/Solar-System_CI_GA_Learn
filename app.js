a3179483c649f2a3edb9b8cbe2bbcafcaa9f01e6
SJWarrior17 <[onlyswapniljain@gmail.com]>
2026-05-20 20:43:14 +0530

    20: testing again

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(cors())
 mongoose.connect(process.env.MONGO_URI, {
     // user: process.env.MONGO_USERNAME,
     // pass: process.env.MONGO_PASSWORD,
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }, function(err) {
     if (err) {
         console.log("error!! " + err)
